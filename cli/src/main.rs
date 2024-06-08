e0d9383034c72943ecdaeb66201d12ee842a5162
seth <rader.seth@gmail.com>
2024-06-08 12:49:29 -0400

    info and play features

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,281 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{Sample, SampleFormat, SampleRate};
+use quadio_core as core;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
+
+const ARGUMENTS: [&str; 2] = ["in", "out"];
+
+type CommandArgs = HashMap<&'static str, String>;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum CommandKind {
+    Info,
+    Play,
+    Help,
+}
+
+impl TryFrom<&str> for CommandKind {
+    type Error = String;
+
+    fn try_from(from: &str) -> Result<CommandKind, Self::Error> {
+        match from {
+            "info" => Ok(CommandKind::Info),
+            "play" => Ok(CommandKind::Play),
+            "help" => Ok(CommandKind::Help),
+            other => Err(format!("Unknown sub-command \"{}\"", other)),
+        }
+    }
+}
+
+type Command = (CommandKind, CommandArgs);
+
+fn parse_arg_param(arg_param: &str) -> Result<(&'static str, String), String> {
+    let mut arg_param_iter = arg_param.splitn(2, '=');
+    let arg_slice = arg_param_iter.next().unwrap();
+    let param = arg_param_iter.next().map(String::from).unwrap_or("".into());
+
+    let arg = ARGUMENTS.into_iter().find(|&s| s == arg_slice);
+
+    match arg {
+        None => Err(format!("Unrecognized argument {}", arg_slice)),
+        Some(a) => Ok((a, param)),
+    }
+}
+
+fn parse_args<'a, T: Iterator<Item = &'a str>>(
+    mut args: T,
+) -> Result<Command, String> {
+    let cmd = args
+        .next()
+        .map(|cmd| cmd.try_into())
+        .ok_or(String::from("Missing sub-command"))
+        .and_then(|x| x)?;
+
+    let mut map = HashMap::new();
+    let mut reached_end = false;
+    let mut reached_divider = false;
+
+    while !reached_end {
+        if let Some(arg) = args.next() {
+            if arg.starts_with('-') && !reached_divider {
+                if arg == "--" {
+                    reached_divider = true;
+                } else {
+                    let arg = arg.trim_start_matches('-');
+                    let (argname, param) = parse_arg_param(arg)?;
+                    map.insert(argname, param);
+                }
+            } else {
+                map.insert("in", arg.into());
+                reached_end = true;
+            }
+        } else {
+            reached_end = true;
+        }
+    }
+
+    if let (Some(last), true) = (args.next(), reached_end) {
+        return Err(format!("Unrecognized argument \"{}\"", last));
+    }
+
+    Ok((cmd, map))
+}
+
+fn expect_arg<'a>(
+    args: &'a CommandArgs,
+    argname: &str,
+) -> Result<&'a String, String> {
+    args.get(argname).ok_or_else(|| {
+        if argname == "in" {
+            "No input file provided".into()
+        } else {
+            format!("Expected argument \"{}\"", argname)
+        }
+    })
+}
+
+fn run_command((cmd, args): Command) -> Result<(), String> {
+    if cmd == CommandKind::Help {
+        eprintln!("Help!?")
+    } else {
+        let inpath = Path::new(expect_arg(&args, "in")?);
+        let file = fs::File::open(inpath).map_err(|e| e.to_string())?;
+        let reader = io::BufReader::new(file);
+
+        match cmd {
+            CommandKind::Info => {
+                let info = core::QWaveReader::new(reader)?.metadata();
+                println!("Information");
+                println!("\tChannels = {}", info.spec.channels);
+                println!("\tSample rate = {}", info.spec.sample_rate);
+                println!("\tSample bits = {}", info.spec.bits_per_sample);
+
+                let duration_s =
+                    f64::from(info.duration) / f64::from(info.spec.sample_rate);
+
+                println!(
+                    "\tDuration = {} samples ({:.3}s)",
+                    info.duration, duration_s,
+                );
+
+                match info.cue {
+                    Some(c) => {
+                        let cue_time = f64::from(c.sample_offset)
+                            / f64::from(info.spec.sample_rate);
+
+                        println!(
+                            "\tLoop at sample {} ({:.3}s)",
+                            c.sample_offset, cue_time,
+                        );
+                    }
+                    None => println!("No loop point found"),
+                }
+            }
+            CommandKind::Play => {
+                let host = cpal::default_host();
+
+                let device = host
+                    .default_output_device()
+                    .ok_or("Output device not found")?;
+
+                let mut wave_reader = core::QWaveReader::new(reader)?;
+                let metadata = wave_reader.metadata();
+                let loop_sample = metadata.cue.map(|c| c.sample_offset);
+
+                if let Some(s) = loop_sample {
+                    if s > metadata.duration {
+                        return Err("Loop sample exceeds file duration".into());
+                    }
+                }
+
+                let desired_rate = metadata.spec.sample_rate;
+
+                let duration = match metadata.cue {
+                    None => Some(Duration::from_millis(
+                        (metadata.duration * 1000 / desired_rate + 1).into(),
+                    )),
+                    Some(_) => None,
+                };
+
+                let config = device
+                    .supported_output_configs()
+                    .map_err(|e| e.to_string())?
+                    .filter(|cfg| {
+                        cfg.channels() == 1
+                            && cfg.sample_format() == SampleFormat::F32
+                    })
+                    .map(|cfg| {
+                        cfg.try_with_sample_rate(SampleRate(desired_rate))
+                    })
+                    .next()
+                    .ok_or("Could not find appropriate configuration")?
+                    .ok_or(
+                        "Could not acquire stream with desired sample rate",
+                    )?;
+
+                let samples = wave_reader.collect_samples()?;
+
+                println!("Playing. . .");
+
+                let mut offset = 0usize;
+
+                let paint_samples = move |buf: &mut [f32], _: &'_ _| {
+                    let samples_len = samples.len();
+                    let in_end = samples_len.min(offset + buf.len());
+                    let sample_ct = in_end.saturating_sub(offset);
+
+                    if let (Some(loop_start), true) = (loop_sample, in_end != 0)
+                    {
+                        let loop_start = loop_start as usize;
+                        let loop_len = samples_len - loop_start;
+
+                        let wrap = |off: usize| {
+                            if off > samples_len {
+                                (off - loop_start) % loop_len + loop_start
+                            } else {
+                                off
+                            }
+                        };
+
+                        let mut write_start = 0usize;
+                        let mut write_end;
+
+                        loop {
+                            let write_count = sample_ct
+                                .min(buf.len() - write_start)
+                                .min(samples_len - offset);
+
+                            write_end = write_start + write_count;
+                            let read_end = offset + write_count;
+
+                            buf[write_start..write_end]
+                                .copy_from_slice(&samples[offset..read_end]);
+
+                            offset += sample_ct;
+                            offset = wrap(offset);
+                            write_start += write_count;
+
+                            if write_start >= buf.len() {
+                                break;
+                            }
+                        }
+                    } else if offset < samples_len {
+                        let write_count =
+                            sample_ct.min(buf.len()).min(samples_len - offset);
+
+                        let read_end = offset + write_count;
+
+                        buf[..write_count]
+                            .copy_from_slice(&samples[offset..read_end]);
+
+                        buf[write_count..].fill(f32::EQUILIBRIUM);
+                        offset += buf.len();
+                    } else {
+                        buf.fill(f32::EQUILIBRIUM);
+                    }
+                };
+
+                let stream = device
+                    .build_output_stream(
+                        &config.into(),
+                        paint_samples,
+                        move |_| {},
+                        duration,
+                    )
+                    .map_err(|e| e.to_string())?;
+
+                stream.play().unwrap();
+
+                if let Some(duration) = duration {
+                    sleep(duration);
+                } else {
+                    loop {
+                        sleep(Duration::from_millis(10));
+                    }
+                }
+
+                println!("Done.");
+            }
+            CommandKind::Help => {}
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args_owned: Vec<String> = env::args().skip(1).collect();
+    let args = args_owned.iter().map(|arg| &arg[..]);
+
+    let result = parse_args(args);
+
+    if let Err(e) = result.and_then(run_command) {
+        eprintln!("{}", e);
+    }
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,103 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use hound::SampleFormat;
+use std::io::{Read, Seek};
+
+#[derive(Clone)]
+pub struct Metadata {
+    pub spec: hound::WavSpec,
+    pub duration: u32,
+    pub cue: Option<cuet::CuePoint>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct QWaveReader<R: Read> {
+    pub reader: hound::WavReader<R>,
+    pub cue: Option<cuet::CuePoint>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<R: Read + Seek> QWaveReader<R> {
+    pub fn new(reader: R) -> Result<Self, String> {
+        let mut cursor =
+            cuet::WaveCursor::new(reader).map_err(|e| e.to_string())?;
+
+        let cue_bytes = cursor
+            .read_next_chunk_body(*b"cue ")
+            .map_err(|e| e.to_string())?;
+
+        let cue_points = cue_bytes
+            .map(|bytes| cuet::parse_cue_points(&bytes[..]))
+            .unwrap_or(Vec::new());
+
+        let reader = hound::WavReader::new(
+            cursor.restore_cursor().map_err(|e| e.to_string())?,
+        )
+        .map_err(|e| e.to_string())?;
+
+        Ok(QWaveReader {
+            reader,
+            cue: if cue_points.is_empty() {
+                None
+            } else {
+                Some(cue_points[0])
+            },
+        })
+    }
+}
+
+impl<R: Read> QWaveReader<R> {
+    pub fn metadata(&self) -> Metadata {
+        Metadata {
+            spec: self.reader.spec(),
+            duration: self.reader.duration(),
+            cue: self.cue,
+        }
+    }
+}
+
+impl<R: Read> QWaveReader<R> {
+    pub fn collect_samples(&mut self) -> Result<Vec<f32>, String> {
+        let mut error = Option::<String>::None;
+        let spec = self.metadata().spec;
+
+        if spec.channels != 1 {
+            return Err("Too many channels".into());
+        }
+
+        if spec.sample_format != SampleFormat::Int {
+            return Err("Float samples are unsupported".into());
+        }
+
+        fn samp8_to_float(s: i16) -> f32 {
+            (s - i16::from(u8::MAX / 2)) as f32
+                / (i16::from(u8::MAX) * 2) as f32
+        }
+
+        fn samp16_to_float(s: i16) -> f32 {
+            s as f32 / i16::MAX as f32
+        }
+
+        let samp_to_float = if spec.bits_per_sample == 8 {
+            samp8_to_float
+        } else if spec.bits_per_sample == 16 {
+            samp16_to_float
+        } else {
+            return Err("Samples must be 8- or 16-bits".into());
+        };
+
+        let samples = self
+            .reader
+            .samples::<i16>()
+            .map_while(|s| match s {
+                Ok(s) => Some(samp_to_float(s)),
+                Err(e) => {
+                    error = Some(e.to_string());
+                    None
+                }
+            })
+            .collect();
+
+        if let Some(e) = error {
+            Err(e)
+        } else {
+            Ok(samples)
+        }
     }
 }
